7f3102eb0d6fb1f0af314ae439ba17528c9295ef
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-29 23:57:33 +0200

    Dynamic Routing and Redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -11,15 +11,20 @@ function App() {
         <MainHeader></MainHeader>
       </header>
       <main>
-        <Route path="/welcome">
-          <Welcome></Welcome>
-        </Route>
-        <Route path="/products">
-          <Products></Products>
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail></ProductDetail>
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to='/welcome'></Redirect>
+          </Route>
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = (props) => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A book</li>
-        <li>An iPhone 13</li>
-        <li>A Tesla Car</li>
+        <li>
+          <Link to="/products/book">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/iphone">An iPhone</Link>
+        </li>
+        <li>
+          <Link to="/products/tesla">A Tesla Car</Link>
+        </li>
       </ul>
     </section>
   );
